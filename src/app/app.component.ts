995bcd196f5d14a7cad3c4cb5f6088730ac0febc
Muso Verda <gearmobile@gmail.com>
2024-08-26 22:34:51 +0300

    feat: :sparkles: Use view container ref

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ComponentRef,
+  inject,
+  viewChild,
+  ViewContainerRef,
+} from '@angular/core';
+import { WidgetComponent } from './components/widget/widget.component';
 
 @Component({
   selector: 'dc-root',
@@ -9,11 +17,21 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
+  private widgetContainerRef = viewChild('widget', { read: ViewContainerRef });
+  private widgetComponentRef!: ComponentRef<WidgetComponent>;
+
   public createComponent(): void {
-    //
+    this.widgetContainerRef()?.clear(); // очищаем все старые динамические компоненты
+    this.widgetComponentRef =
+      this.widgetContainerRef()!.createComponent(WidgetComponent);
   }
 
   public destroyComponent(): void {
-    //
+    // => обращение к ссылке на компонент WidgetComponent
+    // this.widgetComponentRef.destroy();
+
+    // => обращение к ссылке на контейнер компонента WidgetComponent
+    // this.widgetContainerRef()?.remove(2);
+    this.widgetContainerRef()?.clear();
   }
 }
